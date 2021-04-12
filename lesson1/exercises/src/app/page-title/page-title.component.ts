b7324e2194ca02487897cee602df1d13d3f71ee0
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-04-12 15:41:23 -0500

    Finished - added background color.

--- lesson1/exercises/src/app/page-title/page-title.component.ts
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-title.component.css']
 })
 export class PageTitleComponent implements OnInit {
-  pageTitle = 'Page Title';
+  title = 'Christina`s First Angular Page';
   constructor() { }
 
   ngOnInit() {
