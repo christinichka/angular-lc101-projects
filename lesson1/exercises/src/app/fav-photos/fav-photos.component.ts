3834842c222a38068e9f638f4b2ed0c8ada3a94e
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-04-12 13:35:36 -0500

    Angular, Part 1 Exercises Part 1: Modify the CSS

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Fantastic Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwjaXAqNFsDBr2OUkYFD_0mGRGRqFuUIv0qA&usqp=CAU';
+  image3 = 'https://cdn.britannica.com/s:800x450,c:crop/66/195966-138-F9E7A828/facts-turtles.jpg';
 
   constructor() { }
 
